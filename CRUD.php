691f9382a1393c4156fde6d1fbf805509e426949
Dhurv Prajapati <159237463+dhruvb2007@users.noreply.github.com>
2025-04-08 20:58:40 +0530

    Create CRUD.php

---
+++ CRUD.php
@@ -0,0 +1,23 @@
+$conn = mysqli_connect("localhost", "root", "", "test");
+
+// INSERT
+$name = "Dhruv";
+$sql = "INSERT INTO users (name) VALUES ('$name')";
+mysqli_query($conn, $sql);
+
+// SELECT
+$sql = "SELECT * FROM users";
+$result = mysqli_query($conn, $sql);
+while ($row = mysqli_fetch_assoc($result)) {
+    echo $row['name'] . "<br>";
+}
+
+// UPDATE
+$sql = "UPDATE users SET name='Dhruv Updated' WHERE id=1";
+mysqli_query($conn, $sql);
+
+// DELETE
+$sql = "DELETE FROM users WHERE id=1";
+mysqli_query($conn, $sql);
+
+mysqli_close($conn);
